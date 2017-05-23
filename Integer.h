ab67523ef292c27dfe5d9489af23b0aee148f33e
Carlos <alfreoldoriverac25@gmail.com>
2017-05-23 10:12:03 -0600

    fin

--- Integer.h
+++ Integer.h
@@ -9,12 +9,20 @@ class Integer{
 		Integer();
 		int getValue();
 		void setValue(int);
+		
 		int operator+ (int rvalue){
-			return entero+rvalue;
+			return this->entero+rvalue;
 		}
+
 		double operator+(double rvalue){
-			return entero+rvalue;
+			return this->entero+rvalue;
 		}
+
+		bool operator==(Integer& rvalue){
+			return this->entero==rvalue.getValue();
+		}
+
+		operator int(){return entero;}
 };
 
 #endif
--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,16 @@ using namespace std;
 
 int main(){
 	Integer entero(100);
-
+	Integer entero2(100);
 	cout<<"El entero es: "<<entero+10.8<<endl;
 
+	if (entero==entero2){
+		cout<<"Son iguales "<<endl;
+	}
+
+	//static cast
+	cout<<static_cast<int>(entero)<<endl;
+	
 	//int i=0;
 	//cout<< i++;
 
